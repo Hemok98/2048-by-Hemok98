142d2acc0a6f0aae991aafba7f154103781302f1
Hemok98 <hemok98@yandex.ru>
2019-04-24 19:40:02 +0200

    Начато

---
+++ 2048 by Hemok98/Container/Skills.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2048_by_Hemok98.Container
+{
+    partial class Cells_container
+    {
+
+    }
+}

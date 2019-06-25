176eea767e3fca1b8ec342af48f15dd3b238f524
Hemok98 <hemok98@yandex.ru>
2019-06-25 08:35:53 +0200

    Написаны коментарии для MainForm.Cs

--- 2048 by Hemok98/Form/MainPanel/MainForm.cs
+++ 2048 by Hemok98/Form/MainPanel/MainForm.cs
@@ -13,22 +13,23 @@ namespace _2048_by_Hemok98
 {
     public partial class MainForm : Form
     {
-        private bool showNew = false;
+        private bool showNew = false; //переменная отвечающая за отображение новых ячеек
 
-        private bool canUseKeys = true;
+        private bool canUseKeys = true; //отвечает за возможность управлять игрой через WASD
 
-        private int displayCellsCount = 4;
+        private int displayCellsCount = 4; //хранит кол-во ячеек поля
 
-        private int selectedPanel = 1;
+        private int selectedPanel = 1; //хранит номер использованной панельки
 
-        public MainForm()
+        public MainForm() //конструктор класса новой формы, то что запустится при её создании
         {
+            //инициализация объектов на форме
             this.InitializeComponent();
             this.MyInitializeComponent();
             this.InitializeOptionsPanel();
             this.IntitializeSavesPanel();
             this.SetDisplayOption();
-            IntitializeLoadPanel();
+            this.IntitializeLoadPanel();
 
             /*
             Properties.Settings.Default.saveString1 = "";
@@ -42,20 +43,21 @@ namespace _2048_by_Hemok98
             Properties.Settings.Default.saveString9 = "";
             */
 
+            //старт игры
             this.game.RestartGame();
             this.game.Output(this.cellsDispay, stepDisplay, scoreDisplay, recordDisplay, this.x2PriceDisplay, this.deletePriceDisplay,this.backPriceDisplay);
 
 
         }
 
-        private Game game = new Game();
+        private Game game = new Game(); //сама игра по факту
 
-        private void MoveButtonClick(object sender, EventArgs e)
+        private void MoveButtonClick(object sender, EventArgs e) //функция обработки нажатия на клавиши управления
         {
             int x = -1, y = -1;
-            Button pressedButton = (Button)sender;
+            Button pressedButton = (Button)sender; //смотрим какая клавиша была нажата
 
-            switch (pressedButton.Name)
+            switch (pressedButton.Name) //выполняем движение
             {
                 case "rightButton":
                 {
@@ -82,39 +84,39 @@ namespace _2048_by_Hemok98
                 }
             }
 
-            if (x != -1)
+            if (x != -1) //если оно выполнено, то выводим поле и всё остальное
             {
                 this.game.Output(this.cellsDispay, stepDisplay, scoreDisplay, recordDisplay, this.x2PriceDisplay, this.deletePriceDisplay, this.backPriceDisplay);
-                ShowNewCell(x, y);
+                this.ShowNewCell(x, y);
                 
             }
             
         }
 
-        private void RestartButtonClick(object sender, EventArgs e)
+        private void RestartButtonClick(object sender, EventArgs e) //перезапуск игры
         {
             this.game.RestartGame();
             this.game.Output(this.cellsDispay, stepDisplay, scoreDisplay, recordDisplay, this.x2PriceDisplay, this.deletePriceDisplay, this.backPriceDisplay);
         }
 
-        private async void CellsDisplayClick(object sender, EventArgs e)
+        private async void CellsDisplayClick(object sender, EventArgs e) //обработка нажатия по игровому полю
         {
             TButton cell = (TButton)sender;
             int indexX = int.Parse(cell.Name[0] + "");
             int indexY = int.Parse(cell.Name[1] + "");
             
-            if ( this.game.UseSkill(indexX,indexY) )
+            if ( this.game.UseSkill(indexX,indexY) ) //смотрим смогли мы использовать скил по этому полю
             {
-                await Task.Run(() => ChangeButtonColor(cell, System.Drawing.Color.Green));
-                this.game.Output(this.cellsDispay, stepDisplay, scoreDisplay, recordDisplay, this.x2PriceDisplay, this.deletePriceDisplay,this.backPriceDisplay);
+                await Task.Run(() => ChangeButtonColor(cell, System.Drawing.Color.Green)); //в паралельном потоке меняем цвет нажатой ячейки на зелёный
+                this.game.Output(this.cellsDispay, stepDisplay, scoreDisplay, recordDisplay, this.x2PriceDisplay, this.deletePriceDisplay,this.backPriceDisplay); //выводим
             }
             else
             {
-                await Task.Run(() => ChangeButtonColor(cell, System.Drawing.Color.Red));
+                await Task.Run(() => ChangeButtonColor(cell, System.Drawing.Color.Red)); //т.к. скил не выполнился, то в паралельном потоке меняем цвет на красный
             }
         }
 
-        private void ChangeButtonColor(TButton cell, System.Drawing.Color color)
+        private void ChangeButtonColor(TButton cell, System.Drawing.Color color) //меняет цвет ячейки на заданный, а потом возвращает его обратно
         {
             cell.BackColor = color;
             Thread.Sleep(200);
@@ -122,37 +124,39 @@ namespace _2048_by_Hemok98
             cell.BackColor = cell.mainColor;
         }
 
-        private async void ShowNewCell(int x, int y)
+        private async void ShowNewCell(int x, int y) //показывает новую появившуюся ячейку
         {
             if (this.showNew) await Task.Run(() => ChangeButtonColor(this.cellsDispay[y,x], System.Drawing.Color.LightBlue));
         }
 
-        private void X2ButtonClick(object sender, EventArgs e)
+        private void X2ButtonClick(object sender, EventArgs e) //нажатие по Х2
         {
             this.game.SelectActivatedSkill(Skills.X2);
             //this.textBox1.Text += this.game.skillActivated.ToString();
         }
 
-        private void DeleteButtonClick(object sender, EventArgs e)
+        private void DeleteButtonClick(object sender, EventArgs e) //нажатие по обнулению
         {
             this.game.SelectActivatedSkill(Skills.DELETE);
         }
 
-        private void BackButtonClick(object sender, EventArgs e)
+        private void BackButtonClick(object sender, EventArgs e) //нажатие по ходу назад
         {
             this.game.SelectActivatedSkill(Skills.BACK);
             this.game.Output(this.cellsDispay, stepDisplay, scoreDisplay, recordDisplay, this.x2PriceDisplay, this.deletePriceDisplay,this.backPriceDisplay);
         }
 
-        private void ChangeShowNewCells(object sender, EventArgs e)
+        private void ChangeShowNewCells(object sender, EventArgs e) //переключать показа ячеек
         {
             this.showNew = !this.showNew;
         }
 
-        private void GoToOptionsClick(object sender, EventArgs e)
+        private void GoToOptionsClick(object sender, EventArgs e) //нажатие по кнопке сверху для перехода во вкладку опций
         {
             if (selectedPanel == 2) return;
+            //смотрим если уже была выбрана эта панелька
 
+            //обнуляем панельку с настройка и отключаем видимость остальных
             this.SetDisplayOption();
             if (this.selectedPanel == 1) this.panel1.Visible = false;
             if (this.selectedPanel == 3) this.panel3.Visible = false;
@@ -162,13 +166,14 @@ namespace _2048_by_Hemok98
             this.panel2.Visible = true;
         }
 
-        private void KeyPressed (object sender, KeyEventArgs e)
+        private void KeyPressed (object sender, KeyEventArgs e) //обработка нажатий клавиш клавиатуры
         {
             if (!canUseKeys) return;
+            //проверка на использование клавиш
 
             bool beenUsed = false;
             int x = -1, y = -1;
-            switch (e.KeyData)
+            switch (e.KeyData) //смотрим  какая клавиша была нажата
             {
                 case Keys.W:
                 {
@@ -199,21 +204,22 @@ namespace _2048_by_Hemok98
                 }
             }
 
-            if (x != -1 && beenUsed)
+            if (x != -1 && beenUsed) //если была нажата нужная клавиша и что-либо исполнилось, то выводим и отображаем новую
             {
                 this.game.Output(this.cellsDispay, stepDisplay, scoreDisplay, recordDisplay, this.x2PriceDisplay, this.deletePriceDisplay, this.backPriceDisplay);
-                ShowNewCell(x, y);
+                this.ShowNewCell(x, y);
             }
 
         }
 
-        private void Form1Closed(object sender, FormClosedEventArgs e)
+        private void Form1Closed(object sender, FormClosedEventArgs e) //обработка закрытия формы(программы в целом)
         {
-            Properties.Settings.Default.saveRecord = this.game.GetRecord();
-            Properties.Settings.Default.Save();
+            Properties.Settings.Default.saveRecord = this.game.GetRecord(); //сохраняем рекорд в настройки
+            Properties.Settings.Default.Save(); //сохраняем настройки
         }
 
-        private void GoToMainPanelClick(object sender, EventArgs e)
+        private void GoToMainPanelClick(object sender, EventArgs e) //то же что и GoToOptionsClick. Нужно переписать в единую функцию 
+        // fix me
         {
             if (selectedPanel == 1) return;
             
@@ -225,7 +231,7 @@ namespace _2048_by_Hemok98
             this.panel1.Visible = true;      
         }
 
-        private void GoToSavePanelClick(object sender, EventArgs e)
+        private void GoToSavePanelClick(object sender, EventArgs e) //аналогично предыдущей
         {
             if (selectedPanel == 3) return;
            
@@ -240,7 +246,7 @@ namespace _2048_by_Hemok98
             this.selectedPanel = 3;
         }
 
-        private void GoToLoadPanelClick(object sender, EventArgs e)
+        private void GoToLoadPanelClick(object sender, EventArgs e)//аналогично предыдущей
         {
             if (selectedPanel == 4) return;
             
